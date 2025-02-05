34adef3a8bf2b29bd3d5df6eac1d1929bb6d390f
FJ703 <fxj5059@psu.edu>
2025-02-04 22:11:23 -0500

    Add files via upload

---
+++ WebApp/src/main/java/WebApp.java
@@ -0,0 +1,41 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/index")
+/**
+ * Servlet implementation class WebApp
+ */
+public class WebApp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WebApp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect("index.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ WebApp/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1><center>Salesforce Clinical Rotation</center></h1>
+<h2><center> Created by: Taylor Willis, Samantha Dennison, Faiza Jilani</center></h2>
+<p><center>Optimizing Successful Mentorships Among Preceptors and Medical Students from Underserved Communities
+for AHEC (Area Health Education Center) by Generating a Matching Algorithm</center></p>
+</body>
+</html>
